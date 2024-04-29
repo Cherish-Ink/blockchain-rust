9f196a6894fb74466f468401f8d5f5a7828c0619
Cherish <95302841+Cherish-Ink@users.noreply.github.com>
2024-04-29 17:53:19 +0800

    Update blockchain.rs

--- core/src/blockchain.rs
+++ core/src/blockchain.rs
@@ -5,17 +5,17 @@ pub struct BlockChain {
 }
 
 impl BlockChain {
-    pub fn add_block(&mut self, data: String) {
+    pub fn add_block(&mut self, data: String) {  //向区块链中添加一个新的区块
         let pre_block = &self.blocks[self.blocks.len() - 1];
         let new_block = block::Block::new_block(data, pre_block.hash.clone());
         self.blocks.push(new_block);
     }
 
-    fn new_genesis_block() -> block::Block {
+    fn new_genesis_block() -> block::Block {  //创建一个创世块
         block::Block::new_block("This is genesis block".to_string(), String::from(""))
     }
 
-    pub fn new_blockchain() -> BlockChain {
+    pub fn new_blockchain() -> BlockChain {  //创建一个新区块链
         BlockChain {
             blocks: vec![BlockChain::new_genesis_block()],
         }
