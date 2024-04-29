f5982dc92ab577d29f51e89195b629ad17a2cd21
Cherish <95302841+Cherish-Ink@users.noreply.github.com>
2024-04-29 17:49:49 +0800

    Update block.rs

--- core/src/block.rs
+++ core/src/block.rs
@@ -3,26 +3,26 @@ use utils::coder;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-pub struct BlockHeader {
+pub struct BlockHeader {  //区块头
     pub time: i64,  //时间戳
     pub tx_hash: String,  //交易数据merkle数根hash值
     pub pre_hash: String, //前一区块（父区块）hash值
 }
 
 #[derive(Debug)]
-pub struct Block {
+pub struct Block {  //区块
     pub header: BlockHeader,  //区块头部信息
     pub hash: String,  //当前区块hash值
     pub data: String, //交易数据
 }
 
 impl Block {
-    fn set_hash(&mut self) {
+    fn set_hash(&mut self) {  //设置hash值
         let header = coder::my_serialize(&(self.header));
         self.hash = coder::get_hash(&header[..]);
     }
 
-    pub fn new_block(data: String, pre_hash: String) -> Block {
+    pub fn new_block(data: String, pre_hash: String) -> Block {  //创建一个新的区块
         let transactions = coder::my_serialize(&data);
         let tx_hash = coder::get_hash(&transactions[..]);
 
